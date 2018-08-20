a62dc6f704eaf3690d72fc23cdabec53cd11a46d
Brendan Shea and William Wittenbrock <william@>
2018-08-20 14:01:56 -0700

    Add location, dates, meal input capabilities.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 // BACKEND
 
-function Review(name) {
+function Review(name, location, date, meal) {
   this.name = name;
+  this.location = location;
+  this.date = date;
+  this.meal = meal;
 }
 
 function resetFields() {
   $("#name").val("");
+  $("#location").val("");
+  $("#date").val("");
+  $("#meal").val("");
 }
 
 // FRONT END
@@ -15,14 +21,20 @@ $(document).ready(function(){
     event.preventDefault();
 
     var inputtedName = $("input#name").val();
-    console.log(inputtedName);
-    var newReview = new Review(inputtedName);
+    var inputtedLocation = $("input#location").val();
+    var inputtedDate = $("input#date").val();
+    var inputtedMeal = $("input#meal").val();
+
+    var newReview = new Review(inputtedName, inputtedLocation, inputtedDate, inputtedMeal);
 
     $("ul#restaurant-names").append("<li><span class='restaurant-names'>" + newReview.name + "</span></li>");
 
   $("#restaurant-names").last().click(function() {
     $("#show-restaurant-reviews").show();
     $("#show-restaurant-reviews h2").text(newReview.name);
+    $(".location").text(newReview.location);
+    $(".date").text(newReview.date);
+    $(".meal").text(newReview.meal);
   });
 
     resetFields();
